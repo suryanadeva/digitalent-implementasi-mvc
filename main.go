ab64f3af5abd577779c1099c752a004530d022d6
suryanadeva <suryanadeva2504@gmail.com>
2020-11-04 21:04:15 +0700

    commit all

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"implementasi-mvc/app/middleware"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"implementasi-mvc/app/config"
+	"implementasi-mvc/app/controller"
+)
+
+func main() {
+	db := config.DBInit()
+
+	accountController := controller.AccountController{DB: db}
+	transactionController := controller.TransactionController{DB: db}
+
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	router.POST("/api/v1/account/add", accountController.CreateAccount)
+	router.POST("/api/v1/login", accountController.Login)
+	router.GET("/api/v1/account", middleware.Auth, accountController.GetAccount)
+
+	router.POST("/api/v1/transfer", middleware.Auth, transactionController.Transfer)
+	router.POST("/api/v1/withdraw", middleware.Auth, transactionController.Withdraw)
+	router.POST("/api/v1/deposit", middleware.Auth, transactionController.Deposit)
+
+	router.Run(":8000")
+}
